2d94c4369c161e0a3a0876cab1d768b6af4b0cd4
coolratfromsunda <105347307+coolratfromsunda@users.noreply.github.com>
2022-07-27 16:05:24 +0700

    Add files via upload

---
+++ Hewan.java
@@ -0,0 +1,16 @@
+public class Hewan {
+
+        // Method dari kelas Hewan
+        void lari() {
+            System.out.println("Berlari dengan sangat cepat.");
+        }
+
+        void jalan() {
+            System.out.println("Berjalan sambil melompat.");
+        }
+
+        void makan() {
+            System.out.println("Makan wortel dengan menggunakan mulutnya");
+        }
+
+}
---
+++ Kelinci.java
@@ -0,0 +1,11 @@
+public class Kelinci {
+    public static void main(String[] args) {
+
+        // Memanggil kelas Hewan
+        Hewan kelinci = new Hewan();
+        kelinci.makan(); // Method atau tingkah laku hewan kelinci
+        kelinci.jalan();
+        kelinci.lari();
+
+    }
+}
